9c1fb41f41e8c64d86ce4774f709be80961efe2f
luckynagarg <227975184+luckynagarg@users.noreply.github.com>
2026-08-08 08:43:34 +0530

    firebase

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const corsAllowedOriginsFromEnv = process.env.CORS_ALLOWED_ORIGINS
   : [];
 
 const defaultAllowedOrigins = [
-  "https://internarea-one.vercel.app/",
+  "https://internarea-one.vercel.app",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
 ];
